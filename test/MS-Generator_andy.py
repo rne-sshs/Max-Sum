cf83514112c65256a84fab6504cf506946e82b44
andyjung <andyjung2104@gmail.com>
2022-05-28 03:56:01 +0900

    feat: 3d-testcases

--- test/MS-Generator_andy.py
+++ test/MS-Generator_andy.py
@@ -1,12 +1,21 @@
 import random
 
-n=5000
-filename = 'okok'
+n=500
+is_3d = True
+
+filename = 'maxsum_3dtest1'
 maxcoor = 1000
 file = open(filename+'.txt','w')
 file.write(str(n)+"\n")
-for i in range(n):
-    r1=random.uniform(0,maxcoor)
-    r2=random.uniform(0,maxcoor)
-    file.write(f"{r1} {r2}\n")
+if not is_3d:
+    for i in range(n):
+        r1=random.uniform(0,maxcoor)
+        r2=random.uniform(0,maxcoor)
+        file.write(f"{r1} {r2}\n")
+else:
+    for i in range(n):
+        r1=random.uniform(0,maxcoor)
+        r2=random.uniform(0,maxcoor)
+        r3=random.uniform(0,maxcoor)
+        file.write(f"{r1} {r2} {r3}\n")
 file.close()
